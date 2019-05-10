e7e8afae057acb13662fcffb0660b88f310f0b50
Pysuper <1821764535@qq.com>
2019-05-10 17:21:42 +0800

    操作淘宝页面

--- spider_18_taobao.py
+++ spider_18_taobao.py
@@ -1,33 +1,38 @@
-import requests
+import time
+
+from selenium import webdriver
 
 
-class TaoBao():
+class Taobao():
+    """淘宝的数据需要登陆后才能获取"""
+
     def __init__(self):
-        self.base_url = "https://s.taobao.com/search?q=macbookpro&imgfile=&commend=all&ssid=s5-e&search_type=item&sourceId=tb.index&spm=a21bo.2017.201856-taobao-item.1&ie=utf8&initiative_id=tbindexz_20170306"
-        self.header = {
-            # "Cookie":""
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"
-        }
+        self.base_url = "https://login.taobao.com/member/login.jhtml?from=taobaoindex&f=top&style=&sub=true&redirect_url=https%3A%2F%2Fi.taobao.com%2Fmy_taobao.htm%3Fspm%3Da21bo.2017.754894437.3.5af911d96dD87y%26ad_id%3D%26am_id%3D%26cm_id%3D%26pm_id%3D1501036000a02c5c3739"
+        self.browser = webdriver.Chrome("./chromedriver")
 
-    def get_url_list(self):
-        return []
+    def selenium_parse(self):
+        self.browser.get(self.base_url)  # 打开页面
 
-    def get_html(self, url):
-        response = requests.get(url, self.header)
-        response.encoding = response.apparent_encoding
-        if response.status_code == 200:
-            return response.text
-        return response.text
+        # 用户登录
+        self.browser.find_element_by_xpath('//i[@class="iconfont static"]').click()
+        self.browser.find_element_by_id('TPL_username_1').send_keys("824回忆这首歌")
+        self.browser.find_element_by_xpath('//input[@class="login-text"]').send_keys("ZXT,960417")
+        # 拖动滑块移动
+        self.browser.find_element_by_xpath('//span[@class="nc-lang-cnt"]')
+        self.browser.find_element_by_xpath('//button[@class="J_Submit"]').click()
 
-    def parse_html(self, html):
-        print(html)
+        time.sleep(1)
+
+        # 输入数值检索
+        self.browser.find_element_by_xpath('//div[@class="search-combobox-input-wrap"]/input').send_keys("MacBookPro")
+        self.browser.find_element_by_xpath('//button[@class="btn-search tb-bg"]').click()
+
+        time.sleep(1)
+        self.browser.close()
 
     def run(self):
-        for url in self.get_url_list():
-            html = self.get_html(url)
-            self.parse_html(html)
+        self.selenium_parse()
 
 
-if __name__ == '__main__':
-    search = TaoBao()
-    search.run()
+taobao = Taobao()
+taobao.run()
