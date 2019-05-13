f6b457e43928f56f9b1bb0fefc653451b6a406d8
Pysuper <1821764535@qq.com>
2019-05-13 15:35:24 +0800

    优化智通人才网岗位数据提取

--- spider_04_zhitong.py
+++ spider_04_zhitong.py
@@ -1,23 +1,29 @@
+import json
 import requests
 from urllib.parse import urlencode
 
 
 class ZhiTong():
     """
-    使用多进程和多线程
+    使用多进程和多线程 ==> 队列 ==> 生产消费模式
     """
+
     def __init__(self):
         # 初始化类属性
         self.base_url = "http://www.job5156.com/s/result/ajax.json?"
-        self.data = {
-            'keyword': input("请输入工作名称："),
-            'keywordType': 0,
-            'locationList': 14010000,
-            'pageNo': 1
-        }
+        self.num = int(input("请输入抓取页数： "))
+        self.keyword = input("请输入工作名称： ")   # 这里的城市可以通过GET城市代码，让用户选择城市
+
 
     def get_url_list(self):
-        return self.base_url + urlencode(self.data)
+        for page in range(1, self.num):
+            data = {
+                'keyword': self.keyword,
+                'keywordType': 0,
+                'locationList': 14010000,
+                'pageNo': page
+            }
+            yield self.base_url + urlencode(data)
 
     def get_html(self, url):
         response = requests.get(url)
@@ -27,18 +33,32 @@ class ZhiTong():
 
     def get_item(self, html):
         # 在这里使用标签选择的方法，提取出item
-        return ""
+        # 这里直接获取当前网页源码， 获取的是字符串， 需要使用json转换成字典格式
+        html_dict = json.loads(html)
+        items = html_dict["page"]["items"]
+        for info in items:
+            item = {
+                "name": info["posName"].replace("<em>", '').replace("</em>", ''),
+                "work_location": info["workLocationList"][0]["provName"]
+                                 + info["workLocationList"][0]['cityName']
+                                 + info["workLocationList"][0]["townName"],
+                "com_name": info["comName"],
+                "other": ",".join(info["taoLabelList"]),  # 这个方法很实用 ==> 直接把列表变成了字符串
+                "money": info["salaryStrByThousandMonth"]
+            }
+            yield item
 
-    def save_items(self, items):
+    def save_items(self, item):
         # 在这里存储数据， 返回数据保存状态
-        return "OK!"
+        print(item)
 
     def run(self):
         for url in self.get_url_list():
             html = self.get_html(url)
-            item = self.get_item(html)
-            self.save_items(item)
+            for item in self.get_item(html):
+                self.save_items(item)
 
 
-zhitong = ZhiTong()
-zhitong.run()
+if __name__ == '__main__':
+    zhi_tong = ZhiTong()
+    zhi_tong.run()
