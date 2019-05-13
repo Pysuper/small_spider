95884253eae66ea9d51051a1c667442b0d6ebf36
Pysuper <1821764535@qq.com>
2019-05-13 15:40:44 +0800

    完善吉他吧网页源码获取时打印在控制台

--- spider_07_xici.py
+++ spider_07_xici.py
@@ -5,14 +5,16 @@ from lxml import etree
 class XiCi():
     def __init__(self):
         self.base_url = "http://www.xicidaili.com/nn/{}"
+        self.num = int(input("请输入抓取页数： "))
         self.header = {
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/69.0.3497.81 Chrome/69.0.3497.81 Safari/537.36"}
         # self.proxy = {"http": "122.193.244.238:9999"} # 可以使用
+
     def get_url_list(self):
-        return [self.base_url.format(page) for page in range(1, 2)]
+        return [self.base_url.format(page) for page in range(1, self.num)]
 
     def get_html(self, url):
-        response = requests.get(url, headers=self.header)   # proxies = self.proxy
+        response = requests.get(url, headers=self.header)  # proxies = self.proxy
         response.encoding = response.apparent_encoding
         if response.status_code == 200:
             return response.text
@@ -29,6 +31,7 @@ class XiCi():
     def parse_items(self, ip_list, port_list):
         for ip, port in zip(ip_list, port_list):
             print(ip.text + ":" + port.text)
+            # 在这里可以在判断当前这个ip使用情况，再有选择性的输出 ==> 随便访问一个网站
 
     def run(self):
         for url in self.get_url_list():
@@ -37,5 +40,6 @@ class XiCi():
             self.parse_items(ip_list, port_list)
 
 
-xici = XiCi()
-xici.run()
+if __name__ == '__main__':
+    xi_ci = XiCi()
+    xi_ci.run()
