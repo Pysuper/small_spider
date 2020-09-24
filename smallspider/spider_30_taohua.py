33e38424b93bb76b58862d7a9bd82741a680f599
Pysuper <zxt@yj0407>
2020-09-24 21:37:34 +0800

    添加asyncio异步抓取信息保存

--- smallspider/spider_30_taohua.py
+++ smallspider/spider_30_taohua.py
@@ -6,14 +6,17 @@
 
 
 import asyncio
+import os
+from pprint import pprint
+import urllib.request
 import aiohttp
 from lxml import etree
-from pprint import pprint
 
 loop = asyncio.get_event_loop()
 
 base_url = "http://taohuazu9.com/"
 start_url = "http://taohuazu9.com/forum.php?gid=190"
+save_path = "/home/zheng/Videos/"
 
 
 async def get_category():
@@ -32,43 +35,111 @@ async def get_category():
                             'category_title': category_title,
                             'category_href': base_url + category_href,
                         }
-                        loop.create_task(parse_detail(category_task))
+                        loop.create_task(parse_detail(session, category_task))
             except Exception as e:
                 print(e)
             await asyncio.sleep(5)
 
 
-async def parse_detail(task):
+async def parse_detail(session, task):
     """获取帖子的详细信息"""
-    async with aiohttp.ClientSession() as session:
-        try:
-            async with session.get(task["category_href"]) as response:
-                html = await response.text()
-                x_html = etree.HTML(html)
+    try:
+        async with session.get(task["category_href"]) as response:
+            html = await response.text()
+            x_html = etree.HTML(html)
 
-                detail_href_list = x_html.xpath("//ul[@id='waterfall']/li//h3/a/@href")
-                detail_title_list = x_html.xpath("//ul[@id='waterfall']/li//h3/a/text()")
-                for detail_href, detail_title in zip(detail_href_list, detail_title_list):
-                    detail_task = {
-                        'category_title': task["category_title"],
-                        'category_href': task["category_href"],
-                        'detail_title': detail_title,
-                        'detail_href': base_url + detail_href
-                    }
-                loop.create_task(download_img(session, detail_task))
-        except Exception as e:
-            print(e)
-        await asyncio.sleep(5)
+            detail_href_list = x_html.xpath("//ul[@id='waterfall']/li//h3/a/@href")
+            detail_title_list = x_html.xpath("//ul[@id='waterfall']/li//h3/a/text()")
+            image_href = x_html.xpath("""//ul[@id='waterfall']/li/div/a[@onclick="atarget(this)"]/img/@src""")[0]
+            for detail_href, detail_title in zip(detail_href_list, detail_title_list):
+                detail_task = {
+                    'category_title': task["category_title"],
+                    'category_href': task["category_href"],
+                    'detail_title': detail_title.replace(" ", "").replace("/", "-"),
+                    'detail_href': base_url + detail_href,
+                    'image_href': image_href,
+                }
+                # pprint(detail_task)
+            loop.create_task(get_torrent_page(session, detail_task))
+    except Exception as e:
+        print(e)
+    await asyncio.sleep(5)
 
 
-async def download_img(session, detail_task):
-    """获取torrent下载链接"""
+async def get_torrent_page(session, detail_task):
+    """获取torrent下载页面"""
     try:
         async with session.get(detail_task["detail_href"]) as response:
             html = await response.text()
             x_html = etree.HTML(html)
-            img_href = x_html.xpath("//span[@initialized='true']/a/@href")
-            pprint(detail_task, img_href)
+            torrent_href = x_html.xpath("""//span[@onmouseover="showMenu({'ctrlid':this.id,'pos':'12'})"]/a/@href""")[0]
+            torrent_name = x_html.xpath("""//span[@onmouseover="showMenu({'ctrlid':this.id,'pos':'12'})"]/a/text()""")[
+                0]
+            torrent_task = {
+                "category_title": detail_task["category_title"],
+                "category_href": detail_task["category_href"],
+                "detail_title": detail_task["detail_title"],
+                "detail_href": detail_task["detail_href"],
+                "image_href": detail_task["image_href"],
+                "torrent_href": base_url + torrent_href,
+                "torrent_name": torrent_name,
+            }
+            # pprint(torrent_task)
+            # print(randam, torrent_task)
+            loop.create_task(get_torrent(session, torrent_task))
+    except Exception as e:
+        print(e)
+    await asyncio.sleep(5)
+
+
+async def get_torrent(session, torrent_task):
+    """获取torrent下载链接"""
+    try:
+        async with session.get(torrent_task["torrent_href"]) as response:
+            html = await response.text()
+            x_html = etree.HTML(html)
+            torrent_href = x_html.xpath("""//a[@onclick="hideWindow('imc_attachad')"]/@href""")[0]
+            file_task = {
+                "category_title": torrent_task["category_title"],
+                "category_href": torrent_task["category_href"],
+                "detail_title": torrent_task["detail_title"],
+                "detail_href": torrent_task["detail_href"],
+                "torrent_href": torrent_task["torrent_href"],
+                "torrent_name": torrent_task["torrent_name"],
+                "image_href": torrent_task["image_href"],
+                "file_url": torrent_href
+            }
+            # pprint(file_task)
+            loop.create_task(save_torrent(session, file_task))
+    except Exception as e:
+        print(e)
+    await asyncio.sleep(5)
+
+
+async def save_torrent(session, file_task):
+    try:
+        image_name = file_task["detail_title"] + ".jpg"
+        torrent_name = file_task["torrent_name"]
+
+        file_save_path = save_path + file_task["category_title"] + "/" + file_task["detail_title"] + "/"
+        if not os.path.exists(file_save_path):
+            os.makedirs(file_save_path)
+
+        # 使用urllib.request
+        urllib.request.urlretrieve(file_task["image_href"], file_save_path + image_name)
+        urllib.request.urlretrieve(file_task["file_url"], file_save_path + torrent_name)
+
+        # async with session.get(file_task["file_url"]) as torrent:
+        #     pprint(file_task)
+        #     with open(file_save_path + torrent_name, "w") as code:
+        #         code.write(torrent.content)
+
+        # a bytes-like object is required, not 'StreamReader'
+        # async with session.get(file_task["image_href"]) as image:
+        #     with open(file_save_path + image_name, "wb") as code:
+        #         code.write(image.content.encode())
+
+        print(os.getpid(), "{} 保存完成！".format(file_task["category_title"] + "--" + file_task["detail_title"]))
     except Exception as e:
         print(e)
     await asyncio.sleep(5)
---
+++ taohua/download.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/9/24 下午8:17
+# @Author  : Zheng Xingtao
+# @File    : download.py
+
+a = "/imc_attachad-ad.html?aid=MTM0MDI3fDAyZjdlMzQ4fDE2MDA5NDk5MzJ8MHwzODQ1NQ%3D%3D"
+b = 'http://taohuazu9.com/'
+
+print(b[:-1] + a)
